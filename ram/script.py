082f550f45a49111bec6f4e92da500695af5784f
SlavaOrlov <slavaorlov1980@gmail.com>
2022-07-29 23:27:09 +1000

    Working script json2db allchars table

--- ram/script.py
+++ ram/script.py
@@ -6,5 +6,21 @@ def json2db():
     with open('ram/rnm_char_data.json') as file:
         data = json.load(file)
 
-    return data[0]
+    for i in data:
+
+        char = AllChars(
+                name = i.get('name'),
+                status = i.get('status'),
+                species = i.get('species'),
+                type = i.get('type'),
+                gender = i.get('gender'),
+                origin = i.get('origin'),
+                location = i.get('location'),
+                image = i.get('image'),
+                url = i.get('url'),
+                created = i.get('created')
+        )
+
+        char.save()
+    return AllChars.objects.filter(name='Rick')
 
